ebd07309812726d470c854cba20be778010623f2
rofaccess <rofaccess@gmail.com>
2013-07-23 10:58:10 -0400

    Mejora de interfaz de entrada con twitter-bootstrap-rails

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  #Para ke redirija a autenticacion de usuario si no ha iniciado sesion	
+  before_filter :authenticate_user!
   def index
   	if user_signed_in?
   		redirect_to :controller=>'posts', :action=>'index'
